1a3d8ccca3771b662ddec96fa2680dfd70e0e536
Sebastian Castillo <sebastiancastillo1402@gmail.com>
2024-10-14 20:30:15 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PlatziFake',
+  webDir: 'dist'
+};
+
+export default config;
